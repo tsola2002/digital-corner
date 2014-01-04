3c3e494e7b5a45f6b847eb9ed308f7592872f05b
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-04 21:08:49 +0000

    adding recent post functionality to the site

--- wp-content/themes/BLANK-Theme/sidebar.php
+++ wp-content/themes/BLANK-Theme/sidebar.php
@@ -14,14 +14,12 @@
             <div class="widget">
                 <h4>Latest Blog Post</h4>
 
-                <div class="date">December 28th, 2013</div>
-                <h5>This just in, dont get gremlins wet!!</h5>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Alias asperiores eius incidunt nam officia perferendis unde?
-                    Dolorum in non repellat saepe voluptatum. Deleniti,
-                    dolore eos facilis libero numquam quas similique?</p>
+                <?php query_posts("post_per_page=1"); the_post(); ?>
+                <div class="date"><?php the_date(); ?></div>
+                <h5><a href="<?php the_permalink(); ?>"></a><?php the_title(); ?></h5>
+                <p><?php the_excerpt(); ?></p>
 
-
+                <?php wp_reset_query(); ?>
             </div><!--end of .widget-->
 
             <div class="widget industry">
