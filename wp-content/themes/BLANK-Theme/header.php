94bec1a6efbc9ddb2a0492b476c7cbd3f74f93b2
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-04 19:06:16 +0000

    testing bootstrap3

--- wp-content/themes/BLANK-Theme/header.php
+++ wp-content/themes/BLANK-Theme/header.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
-	
+
 	<?php if (is_search()) { ?>
 	   <meta name="robots" content="noindex, nofollow" /> 
 	<?php } ?>
@@ -28,6 +28,8 @@
 		         echo ' - page '. $paged; }
 		   ?>
 	</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	
 	<link rel="shortcut icon" href="/favicon.ico">
 
--- wp-content/themes/BLANK-Theme/index.php
+++ wp-content/themes/BLANK-Theme/index.php
@@ -10,6 +10,7 @@
 
 			<div class="entry">
 				<?php the_content(); ?>
+                <button class="btn btn-info">here we go</button>
 			</div>
 
 			<div class="postmetadata">
