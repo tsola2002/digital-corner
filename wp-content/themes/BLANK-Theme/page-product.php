08ec5bbb3dae6b1e521d1abac9aa7a51e3b121ff
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-09 10:04:46 +0000

    page template added

---
+++ wp-content/themes/BLANK-Theme/page-product.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Package: digital-corner
+ * Filename: page-product.php
+ * Author: solidstunna101
+ * Date: 09/01/14
+ * Time: 09:59
+ *
+ */
+?>
+
+<?php
+
+    /*
+    Template: Product-Page
+    */
+
+?>
