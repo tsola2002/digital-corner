7371a83b30027fb7de2da71c80718436101df8f0
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-09 10:09:38 +0000

    page template added(bug-fix)

--- wp-content/themes/BLANK-Theme/page-product.php
+++ wp-content/themes/BLANK-Theme/page-product.php
@@ -1,19 +1,6 @@
 <?php
-/**
- *
- * Package: digital-corner
- * Filename: page-product.php
- * Author: solidstunna101
- * Date: 09/01/14
- * Time: 09:59
- *
- */
-?>
-
-<?php
-
     /*
-    Template: Product-Page
+    Template Name: Product Page
     */
 
 ?>
