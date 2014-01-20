87fd0181a1afbf3ed7ae4d7af3d5367b2bb54453
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-20 09:44:24 +0000

    screenshot for them replaced & page.php revamped

--- wp-content/themes/BLANK-Theme/home.php
+++ wp-content/themes/BLANK-Theme/home.php
@@ -14,4 +14,44 @@
 
 <?php get_sidebar(); ?>
 
+    <section id="main-content" class="col col-lg-6">
+        <h1 class="giant">The Digital Realm</h1>
+
+        <div class="post">
+
+            <h4><a href="#">The future of the digital realm</a></h4>
+
+            <div class="meta">
+                <em>Posted on:</em> April 10th, 2010 &emsp; &emsp;
+                <em>by</em> Omatsola Sobotie
+            </div>
+
+            <div class="entry">
+                <p>Widgets might look easy to make, but they are anything but. The manufacturing of widgets can be bungled in any step of the process. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas [...]</p>
+            </div>
+
+            <a href="#">Continue Reading &rarr;</a>
+
+        </div>
+
+        <div class="post">
+
+            <h4><a href="#">Hello world!</a></h4>
+
+            <div class="meta">
+                <em>Posted on:</em> March 27th, 2010 &emsp; &emsp;
+                <em>by</em> Omatsola Isaac Sobotie
+            </div>
+            <div class="entry">
+                <p>Welcome to WordPress. This is your first post. Edit or delete it, then start blogging!</p>
+            </div>
+
+            <a href="#">Continue Reading &rarr;</a>
+
+        </div>
+    </section><!--end of #main-content-->
+    <div class="col-lg-2">
+
+    </div>
+
 <?php get_footer(); ?>
