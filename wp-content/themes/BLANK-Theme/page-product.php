96b43a5c6fe63def930bf4479fdd1ca3e926c2ce
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-22 10:05:09 +0000

    product page template static content added

--- wp-content/themes/BLANK-Theme/page-product.php
+++ wp-content/themes/BLANK-Theme/page-product.php
@@ -8,8 +8,21 @@
 
 <?php get_header(); ?>
 
-
-
 <?php get_sidebar(); ?>
+
+<section id="main-content" class="col col-lg-6 col-lg-offset-2">
+    <h2 class="meta">Mega Sprocketizer</h2>
+
+    <div class="product-info-box">
+        <img src='_/img/prod-sprocketizer-large.png'/>
+        <ul>
+            <li><h5>Price</h5> $519.00</li>
+            <li><h5>Product Code</h5> WGA-998-d</li>
+            <li><h5>Dimensions</h5> 3' x 3'</li>
+        </ul>
+    </div>
+
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+</section><!--end of #main-content-->
 
 <?php get_footer(); ?>
