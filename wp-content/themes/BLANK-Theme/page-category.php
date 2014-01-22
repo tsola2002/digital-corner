9695ec60856535fe21a9df1f2db689669b186311
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-22 11:32:29 +0000

    category template created

---
+++ wp-content/themes/BLANK-Theme/page-category.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *
+ * Package: digital-corner
+ * Filename: page-category.php
+ * Author: solidstunna101
+ * Date: 22/01/14
+ * Time: 11:23
+ *
+ */
+?>
+
+<?php
+   /*
+   Template Name: Product-Category
+   */
+?>
+
