41c7e7c0766bd91a12a9325e251b8de2371f4276
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-23 16:41:59 +0000

    single page static content implemented

--- wp-content/themes/BLANK-Theme/single.php
+++ wp-content/themes/BLANK-Theme/single.php
@@ -1,39 +1,36 @@
-<?php get_header(); ?>
+<?php get_header(); the_post(); ?>
 
     <div class="row">
         <div class="col col-lg-12 meta">
-            <h2 id="post-title">Five Ways Digital Products Can Be Bungled</h2>
+            <h2 id="post-title"><?php the_title(); ?></h2>
         </div>
     </div>
 
-    <div class="row">
+    <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+
+    <!--<div class="row">
         <div class="col col-lg-12 meta2">
             <em>Posted on:</em> April 10th, 2010 &emsp; &emsp;
             <em>by</em> Omatsola Isaac Sobotie
         </div>
-    </div>
+    </div>-->
 
 <?php get_sidebar(); ?>
 
     <section id="main-content" class="col col-lg-6">
-        <div class="post">
+        <div <?php post_class() ?> id="post-<?php the_ID(); ?>">
 
             <div class="entry">
 
-                <p>Widgets might look easy to make, but they are anything but. The manufacturing of widgets can be bungled in any step of the process. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor.</p>
+                <?php the_content(); ?>
 
-                <ul>
-                    <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
-                    <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
-                    <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
-                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
-                    <li>nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
-                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
-                    <li>nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
-                </ul>
+                <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
 
+                <?php the_tags( 'Tags: ', ', ', ''); ?>
 
             </div>
+
+            <?php edit_post_link('Edit this entry','','.'); ?>
 
         </div>
     </section><!--end of #main-content-->
