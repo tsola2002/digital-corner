610a4462e35dd653a9c6881c018c653e131c7cf8
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-23 15:37:14 +0000

    making the home page template

---
+++ wp-content/themes/BLANK-Theme/page-bloghome.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Package: digital-corner
+ * Filename: page-bloghome.php
+ * Author: solidstunna101
+ * Date: 23/01/14
+ * Time: 15:34
+ *
+ */
+?>
+<?php
+
+/*
+  Template Name: Page-Bloghome
+  */
+
+?>
+
+<?php get_header(); ?>
+
+
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
