f6f5f26c605e76f234a4a6ed3af3d7eaf9cfba59
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-23 16:29:48 +0000

    single page static content implemented

--- wp-content/themes/BLANK-Theme/single.php
+++ wp-content/themes/BLANK-Theme/single.php
@@ -1,31 +1,46 @@
 <?php get_header(); ?>
 
-	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="row">
+        <div class="col col-lg-12 meta">
+            <h2 id="post-title">Five Ways Digital Products Can Be Bungled</h2>
+        </div>
+    </div>
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-			
-			<h2><?php the_title(); ?></h2>
-			
-			<?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+    <div class="row">
+        <div class="col col-lg-12 meta2">
+            <em>Posted on:</em> April 10th, 2010 &emsp; &emsp;
+            <em>by</em> Omatsola Isaac Sobotie
+        </div>
+    </div>
 
-			<div class="entry">
-				
-				<?php the_content(); ?>
-
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-				
-				<?php the_tags( 'Tags: ', ', ', ''); ?>
-
-			</div>
-			
-			<?php edit_post_link('Edit this entry','','.'); ?>
-			
-		</div>
-
-	<?php comments_template(); ?>
-
-	<?php endwhile; endif; ?>
-	
 <?php get_sidebar(); ?>
+
+    <section id="main-content" class="col col-lg-6">
+        <div class="post">
+
+            <div class="entry">
+
+                <p>Widgets might look easy to make, but they are anything but. The manufacturing of widgets can be bungled in any step of the process. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor.</p>
+
+                <ul>
+                    <li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>
+                    <li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>
+                    <li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+                    <li>nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+                    <li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>
+                    <li>nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>
+                </ul>
+
+
+            </div>
+
+        </div>
+    </section><!--end of #main-content-->
+    <div class="col-lg-2">
+
+    </div>
+
+
 
 <?php get_footer(); ?>
