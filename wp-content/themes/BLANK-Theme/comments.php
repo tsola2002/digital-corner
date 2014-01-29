825d8de814488f16bdc330c6bc19b8f21cb60c72
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-29 10:07:45 +0000

    styling of comments list[3]

--- wp-content/themes/BLANK-Theme/comments.php
+++ wp-content/themes/BLANK-Theme/comments.php
@@ -85,7 +85,7 @@
 			<textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea>
 		</div>
 
-		<div>
+		<div id="submit-button">
 			<input name="submit" type="submit" id="submit" tabindex="5" value="Submit Comment" />
 			<?php comment_id_fields(); ?>
 		</div>
