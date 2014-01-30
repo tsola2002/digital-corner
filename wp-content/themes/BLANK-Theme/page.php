043af58a2b1fc608fb46639d80b85327d753112d
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-01-30 11:43:16 +0000

    fixing up pages[2]

--- wp-content/themes/BLANK-Theme/page.php
+++ wp-content/themes/BLANK-Theme/page.php
@@ -4,28 +4,38 @@
 
 <?php get_sidebar(); ?>
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-    <div class="post" id="post-<?php the_ID(); ?>">
+    <section id="main-content" class="col col-lg-6">
 
-        <h2><?php the_title(); ?></h2>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-        <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
+            <div class="post" id="post-<?php the_ID(); ?>">
 
-        <div class="entry">
+                <h2><?php the_title(); ?></h2>
 
-            <?php the_content(); ?>
+                <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
 
-            <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+                <div class="entry">
 
-        </div>
+                    <?php the_content(); ?>
 
-        <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+                    <?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
+                </div>
+
+                <?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+
+            </div>
+
+            <?php // comments_template(); ?>
+
+        <?php endwhile; endif; ?>
+
+    </section><!--end of #main-content-->
+    <div class="col-lg-2">
 
     </div>
 
-    <?php  comments_template(); ?>
 
-<?php endwhile; endif; ?>
 
 <?php get_footer(); ?>
